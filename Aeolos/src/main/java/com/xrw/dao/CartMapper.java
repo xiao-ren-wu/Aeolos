c91db66a495e78ffe34fde779786637c057ec5ca
xiao-ren-wu <18629015421@163.com>
2018-06-04 23:22:05 +0800

    2018/06/04-完成项目框架的搭建

---
+++ Aeolos/src/main/java/com/xrw/dao/CartMapper.java
@@ -0,0 +1,18 @@
+package com.xrw.dao;
+
+import com.xrw.pojo.Cart;
+
+public interface CartMapper {
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
