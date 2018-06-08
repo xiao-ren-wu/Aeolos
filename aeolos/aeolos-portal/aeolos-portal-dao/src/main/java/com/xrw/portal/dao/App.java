f89718f3d5079d13ff4e6169f28404fed0f7a9c4
xiao-ren-wu <18629015421@163.com>
2018-06-08 20:01:01 +0800

    project init without set file

---
+++ aeolos/aeolos-portal/aeolos-portal-dao/src/main/java/com/xrw/portal/dao/App.java
@@ -0,0 +1,13 @@
+package com.xrw.portal.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ aeolos/aeolos-portal/aeolos-portal-pojo/src/main/java/com/xrw/portal/pojo/App.java
@@ -0,0 +1,13 @@
+package com.xrw.portal.pojo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
