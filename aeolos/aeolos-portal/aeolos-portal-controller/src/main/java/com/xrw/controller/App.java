67864a3df548f168cd240c0b21dc4d513625e023
xiao-ren-wu <18629015421@163.com>
2018-06-08 22:34:14 +0800

    添加了ssm配置文件

---
+++ aeolos/aeolos-portal/aeolos-portal-controller/src/main/java/com/xrw/controller/App.java
@@ -0,0 +1,15 @@
+package com.xrw.controller;
+
+/**
+ * CreateBy IDEA
+ * Description: TODO
+ * Author: teaOrCoffee
+ * CreateDate: 2018/6/8 22:28
+ * UpdateUser:
+ * UpdateDate:
+ * UpdateRemark: TODO
+ * JdkVersion: jdk1.8.0_101
+ */
+
+public class App {
+}
