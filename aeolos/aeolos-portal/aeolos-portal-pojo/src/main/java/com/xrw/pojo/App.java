cecd6250b103c3dabd5f2c025b96d74fa50a240d
xiao-ren-wu <18629015421@163.com>
2018-06-08 17:59:42 +0800

    pojo

---
+++ aeolos/aeolos-portal/aeolos-portal-pojo/src/main/java/com/xrw/pojo/App.java
@@ -0,0 +1,13 @@
+package com.xrw.pojo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
